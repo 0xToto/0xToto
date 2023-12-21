fe31ac82a44c515da7d70c21a8b63703fef79839
0xToto <154510912+0xToto@users.noreply.github.com>
2023-12-21 10:42:56 +0100

    Create README.py

---
+++ README.py
@@ -0,0 +1,20 @@
+class PersonalInfo:
+    def __init__(self):
+        self.username = "TotoHQ"
+        self.age = 18
+        self.location = "France"
+        self.gender = "Male"
+
+class Skills:
+    def __init__(self):
+        self.languages = ["Python","JavaScript","HTML","CSS","PHP"]
+        self.databases = ["MariaDB", "MySQL", "MongoDB"]
+        self.speaking = ["English","French","Spanish","Italian","Portuguese"]
+
+class OtherSkills:
+    def __init__(self):
+        self.skills = ["OSINT","Networking","Cryptography"]
+
+class LearningTopics:
+    def __init__(self):
+        self.topics = ["Pentest","C#","pwn"]
